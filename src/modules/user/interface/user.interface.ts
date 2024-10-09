86d857555a3487163877ac3e28c619fb77075acf
1slom-media <islombektagayev1@gmail.com>
2024-10-09 22:19:36 +0500

    initial

---
+++ src/modules/user/interface/user.interface.ts
@@ -0,0 +1,29 @@
+export interface IUser {
+    _id?: string;
+    phone?: string;
+    name?: string;
+    surname?: string;
+    nickname?: string;
+    email?: string;
+    isAdmin?: boolean;
+    isSeller?: boolean;
+    balance?: number;
+    deposit?: number;
+    paid?: number;
+    avatar?: string;
+    status?: number;
+    region?: number;
+    soldProCount?: number;
+    telegramID?: number;
+    isOperator?: boolean;
+    isBotActive?: boolean;
+    banner?: string;
+    isPhoneActive?: boolean;
+    smsCode?: number;
+    uid?: number;
+    bitcoin?: number;
+    chats?: string[];
+    countSentCode?: number;
+    countSendedSms?: number;
+    ip?: string;
+}
